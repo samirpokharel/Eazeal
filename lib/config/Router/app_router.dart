a2a635572aea1dc69bef588dbb72314160063685
Sameer Pokharel <i.amsameer2061@gmail.com>
2021-10-11 12:04:38 +0545

    style error Screen

--- lib/config/Router/app_router.dart
+++ lib/config/Router/app_router.dart
@@ -18,23 +18,36 @@ class AppRouter {
 
   static Scaffold _error() {
     const String errorAssetPath = "assets/images/error-vector.svg";
-    const String errorMessage = "We were unable to load this page";
+    const String errorMessage =
+        "We were unable to load this page please try to relaunch the app";
     return Scaffold(
-      appBar: AppBar(title: const Text("Something Went wrong!")),
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             SvgPicture.asset(
               errorAssetPath,
               width: 268,
               height: 197,
             ),
-            const SizedBox(height: 20),
+            const SizedBox(height: 30),
             const Text(
-              errorMessage,
+              "Oops! Something went wrong",
               style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w600,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 15),
+            const SizedBox(
+              width: 300,
+              child: Text(
+                errorMessage,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontWeight: FontWeight.w600,
+                ),
               ),
             )
           ],
