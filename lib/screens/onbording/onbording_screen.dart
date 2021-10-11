870ed39579df0d389e3d4417f6565139da12a68d
Sameer Pokharel <i.amsameer2061@gmail.com>
2021-10-11 13:51:49 +0545

    fix indicator first indicator iussue

--- lib/screens/onbording/onbording_screen.dart
+++ lib/screens/onbording/onbording_screen.dart
@@ -12,7 +12,7 @@ class OnbordingScreen extends StatefulWidget {
 
 class _OnbordingScreenState extends State<OnbordingScreen> {
   late PageController _pageController;
-  int page = 0;
+  int currentPage = 0;
   @override
   void initState() {
     _pageController = PageController();
@@ -33,7 +33,7 @@ class _OnbordingScreenState extends State<OnbordingScreen> {
           padding: const EdgeInsets.all(20.0),
           child: Column(
             children: [
-              if (page != onbordings.length - 1)
+              if (currentPage != onbordings.length - 1)
                 Align(
                   alignment: Alignment.topRight,
                   child: _buildSkipButton(onPressed: () {
@@ -49,15 +49,27 @@ class _OnbordingScreenState extends State<OnbordingScreen> {
                 constraints: const BoxConstraints.expand(height: 500),
                 child: PageView.builder(
                   controller: _pageController,
-                  onPageChanged: (int index) {
-                    setState(() => setState(() => page = index));
+                  // onPageChanged: (int page) {
+                  // setState(() {
+                  //     currentPage = page;
+                  // });
+                  // },
+                  onPageChanged: (int page) {
+                    print('page $page');
+
+                    _pageController.animateToPage(
+                      page,
+                      duration: const Duration(milliseconds: 600),
+                      curve: Curves.ease,
+                    );
+                    setState(() => setState(() => currentPage = page));
                   },
                   allowImplicitScrolling: false,
                   physics: const NeverScrollableScrollPhysics(),
                   itemCount: onbordings.length,
                   pageSnapping: true,
                   itemBuilder: (context, index) {
-                    Onbording onbording = onbordings[index];
+                    Onbording onbording = onbordings[currentPage];
                     return Padding(
                       padding: const EdgeInsets.all(20.0),
                       child: Column(
@@ -91,19 +103,20 @@ class _OnbordingScreenState extends State<OnbordingScreen> {
                 children: List.generate(
                   onbordings.length,
                   (index) => _buildIndicator(index, onTap: () {
-                    print(index);
+                    print("Index: $index");
+                    _pageController.animateToPage(
+                      index,
+                      duration: const Duration(milliseconds: 600),
+                      curve: Curves.ease,
+                    );
                     setState(() {
-                      _pageController.animateToPage(
-                        index,
-                        duration: const Duration(milliseconds: 600),
-                        curve: Curves.ease,
-                      );
+                      currentPage = index;
                     });
                   }),
                 ),
               ),
               const SizedBox(height: 30),
-              if (page == onbordings.length - 1)
+              if (currentPage == onbordings.length - 1)
                 MaterialButton(
                   height: 50,
                   minWidth: 200,
@@ -133,7 +146,7 @@ class _OnbordingScreenState extends State<OnbordingScreen> {
   }
 
   Widget _buildIndicator(int index, {required void Function() onTap}) {
-    bool isCurrent = page == index;
+    bool isCurrent = currentPage == index;
     return GestureDetector(
       onTap: onTap,
       child: AnimatedContainer(
