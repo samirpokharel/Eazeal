6c05860535c2689f8facb036ae693af8d5174f54
Sameer Pokharel <i.amsameer2061@gmail.com>
2021-10-12 17:34:40 +0545

    setup onbording wrapper router

--- lib/config/Router/app_router.dart
+++ lib/config/Router/app_router.dart
@@ -3,16 +3,21 @@ import 'package:eazeal/screens/splash/splash_screens.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-
 class AppRouter {
   static Route<CupertinoPageRoute> onGenerateRoute(RouteSettings settings) {
     debugPrint("Route:${settings.name}");
     return CupertinoPageRoute(builder: (_) {
       switch (settings.name) {
+        case Wrapper.routeName:
+          return const Wrapper();
         case SplashScreen.routeName:
           return const SplashScreen();
         case ForgotPassword.routeName:
           return const ForgotPassword();
+        case TokenReciver.routeName:
+          return TokenReciver(
+            tokenReciverType: settings.arguments as TokenReciverType,
+          );
         default:
           return const ErrorScreen();
       }
