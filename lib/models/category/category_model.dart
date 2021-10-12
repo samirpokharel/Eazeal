3bb88eab5ec9547dcad8f320b31ef04501a0f258
Sameer Pokharel <i.amsameer2061@gmail.com>
2021-10-12 21:32:51 +0545

    group bar category model

---
+++ lib/models/category/category_model.dart
@@ -0,0 +1,17 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'dart:convert';
+
+part 'category_model.freezed.dart';
+part 'category_model.g.dart';
+
+@freezed
+abstract class Category with _$Category {
+  const factory Category({
+    required String id,
+    required String categoryName,
+    required int noOfProducts,
+  }) = _Category;
+
+  factory Category.fromJson(Map<String, dynamic> json) =>
+      _$CategoryFromJson(json);
+}
