cde6c5f39a71e2b9d3142f6ff97058341ef88641
Sameer Pokharel <i.amsameer2061@gmail.com>
2021-10-13 11:38:08 +0545

    request interceptor error handler

--- lib/helper/api_helper/request_intercreptor.dart
+++ lib/helper/api_helper/request_intercreptor.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:dio/dio.dart';
+import 'package:eazeal/helper/api_helper/custom_excpetion.dart';
 import 'package:eazeal/helper/api_helper/dio_connectivity_request_retryer.dart';
 
 class RequestInterceptor extends Interceptor {
@@ -10,21 +11,17 @@ class RequestInterceptor extends Interceptor {
     required DioConnectivityRequestRetrier dioConnectivityRequestRetrier,
   }) : _dioConnectivityRequestRetrier = dioConnectivityRequestRetrier;
 
-
-
   @override
   void onError(DioError err, ErrorInterceptorHandler handler) {
     if (_shouldRetry(err)) {
-      try {
-        _dioConnectivityRequestRetrier.scheduleRequestRetry(err.requestOptions);
-      } catch (e) {
-        print(e);
-      }
+      _dioConnectivityRequestRetrier.scheduleRequestRetry(err.requestOptions);
+      throw CustomException(message: "No internet");
+    }
+    if (err.type == DioErrorType.response) {
+      throw CustomException(message: err.response!.data["message"]);
     }
     super.onError(err, handler);
   }
-
-
 
   bool _shouldRetry(DioError err) {
     return err.type == DioErrorType.other &&
