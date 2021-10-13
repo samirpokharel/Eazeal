6fd20544a12de9ee107d1e93ad7a1cdd2f365a3d
Sameer Pokharel <i.amsameer2061@gmail.com>
2021-10-13 11:36:54 +0545

    product controller

---
+++ lib/controller/product_controller.dart
@@ -0,0 +1,68 @@
+import 'package:eazeal/helper/api_helper/custom_excpetion.dart';
+import 'package:eazeal/models/product/product_model.dart';
+import 'package:eazeal/providers.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+abstract class ProductState {}
+
+class ProductInitial extends ProductState {}
+
+class ProductLoading extends ProductState {}
+
+class ProductSuccess extends ProductState {
+  List<Product> products;
+  ProductSuccess({required this.products});
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is ProductSuccess && listEquals(other.products, products);
+  }
+
+  @override
+  int get hashCode => products.hashCode;
+}
+
+class ProductFailed extends ProductState {
+  CustomException exception;
+  ProductFailed({required this.exception});
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is ProductFailed && other.exception == exception;
+  }
+
+  @override
+  int get hashCode => exception.hashCode;
+}
+
+class ProductController extends StateNotifier<ProductState> {
+  final Reader _reader;
+  ProductController({required Reader reader})
+      : _reader = reader,
+        super(ProductInitial());
+
+  void getProduct(String categoryName) async {
+    if (state is ProductLoading) return;
+    try {
+      state = ProductLoading();
+      List<Product> products =
+          await _reader(categoryProvider).getProducts(query: categoryName);
+      if (mounted) {
+        state = ProductSuccess(products: products);
+      }
+    } on CustomException catch (e) {
+      state = ProductFailed(exception: e);
+    }
+  }
+
+  void setInitial() {
+    if (mounted) {
+      state = ProductInitial();
+    }
+  }
+}
